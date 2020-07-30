f4a69b1b26a97f6c835ca781c2174773d4f4f9c8
shanewh1te <shane.white@cireson.com>
2020-07-30 11:08:06 +0100

    Stage 3, Task 1-5
    1. Create GetLetterGrade Override
    2. Top 20 Percent Get an A
    3. Second 20 Percent Get an B
    4. Third 20 Percent Get an C
    5. Fourth 20 Percent Get an D

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +9,39 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            }
+
+            // Find out what 20% of the Students count is, rounds it up and casts it as an integer
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+
+            // Gets the Average Grades of all Students supplied in list form
+            var grades = Students.OrderByDescending(x => x.AverageGrade).Select(x => x.AverageGrade).ToList();
+
+            if (grades[threshold - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+
+            return 'F';
         }
     }
 }
